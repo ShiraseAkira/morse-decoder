1f0fb0c0db2167140b0d1e26b2cbcc2aeb9d3434
ShiraseAkira <justsaynya@gmail.com>
2021-08-16 22:32:21 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    result = '';
+    for (let i = 0; i < expr.length; i += 10){
+        const charExpr = expr.slice(i, i + 10);
+        let charMorse = '';
+        if (charExpr[0] === '*') {
+            result += ' ';
+            continue;
+        }
+
+        for (let j = 0; j < charExpr.length; j += 2) {
+            if (charExpr[j] === '0') {
+                continue;
+            } else if (charExpr[j + 1] === '0') {
+                charMorse += '.';
+            } else {
+                charMorse += '-';
+            }
+        }
+
+        result += MORSE_TABLE[charMorse];
+    }
+
+    return result;
 }
 
 module.exports = {
